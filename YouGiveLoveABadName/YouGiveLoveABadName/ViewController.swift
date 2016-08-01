8ec07a1cd5b36afbe1188e559e3d1dbea7c4f706
JennyShalai <jenny.shalai@gmail.com>
2016-07-31 22:28:56 -0400

    done

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var bonjovi: UIImageView!
     var heightConstraint: NSLayoutConstraint?
+    var isExpanded: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,42 +20,91 @@ class ViewController: UIViewController {
         self.heightConstraint?.active = true
     }
     
-    @IBAction func expandButtonTapped(sender: AnyObject) {
-        
-        UIView.animateKeyframesWithDuration(1, delay: 0, options: .CalculationModeCubic, animations: {
+    func expand() {
+    
+        UIView.animateKeyframesWithDuration(1, delay: 0, options: .CalculationModeLinear, animations: {
             
             UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0.2, animations: {
-                    self.heightConstraint?.active = false
-                    self.heightConstraint = self.bonjovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.5)
-                    self.heightConstraint?.active = true
-                    self.view.layoutIfNeeded()
-                })
+                self.heightConstraint?.active = false
+                self.heightConstraint = self.bonjovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.5)
+                self.heightConstraint?.active = true
+                self.view.layoutIfNeeded()
+            })
             
-                UIView.addKeyframeWithRelativeStartTime(0.2, relativeDuration: 0.6, animations: {
-                    self.heightConstraint?.active = false
-                    self.heightConstraint = self.bonjovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.5)
-                    self.heightConstraint?.active = true
-                    self.view.layoutIfNeeded()
-                })
+            UIView.addKeyframeWithRelativeStartTime(0.2, relativeDuration: 0.6, animations: {
+                self.heightConstraint?.active = false
+                self.heightConstraint = self.bonjovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.4)
+                self.heightConstraint?.active = true
+                self.view.layoutIfNeeded()
+            })
             
-                UIView.addKeyframeWithRelativeStartTime(0.8, relativeDuration: 0.2, animations: {
-                    self.heightConstraint?.active = false
-                    self.heightConstraint = self.bonjovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 1)
-                    self.heightConstraint?.active = true
-                    self.view.layoutIfNeeded()
-                })
+            UIView.addKeyframeWithRelativeStartTime(0.8, relativeDuration: 0.2, animations: {
+                self.heightConstraint?.active = false
+                self.heightConstraint = self.bonjovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 1.1)
+                self.heightConstraint?.active = true
+                self.view.layoutIfNeeded()
+            })
             
-                UIView.addKeyframeWithRelativeStartTime(1, relativeDuration: 0, animations: {
-                    self.heightConstraint?.active = false
-                    self.heightConstraint = self.bonjovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.5)
-                    self.heightConstraint?.active = true
-                    self.view.layoutIfNeeded()
-                })
+            UIView.addKeyframeWithRelativeStartTime(1, relativeDuration: 0, animations: {
+                self.heightConstraint?.active = false
+                self.heightConstraint = self.bonjovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 1)
+                self.heightConstraint?.active = true
+                self.view.layoutIfNeeded()
+            })
             
-            }) { (true) in
-                // after animation code
+        }) { (true) in
+            self.isExpanded = true
+        }
+
+    }
+    
+    
+    func shrink() {
+    
+        UIView.animateKeyframesWithDuration(1, delay: 0, options: .CalculationModeLinear, animations: {
+            
+            UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0.2, animations: {
+                self.heightConstraint?.active = false
+                self.heightConstraint = self.bonjovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 1)
+                self.heightConstraint?.active = true
+                self.view.layoutIfNeeded()
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(0.2, relativeDuration: 0.6, animations: {
+                self.heightConstraint?.active = false
+                self.heightConstraint = self.bonjovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 1.1)
+                self.heightConstraint?.active = true
+                self.view.layoutIfNeeded()
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(0.8, relativeDuration: 0.2, animations: {
+                self.heightConstraint?.active = false
+                self.heightConstraint = self.bonjovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.4)
+                self.heightConstraint?.active = true
+                self.view.layoutIfNeeded()
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(1, relativeDuration: 0, animations: {
+                self.heightConstraint?.active = false
+                self.heightConstraint = self.bonjovi.heightAnchor.constraintEqualToAnchor(self.view.heightAnchor, multiplier: 0.5)
+                self.heightConstraint?.active = true
+                self.view.layoutIfNeeded()
+            })
+            
+        }) { (true) in
+            self.isExpanded = false
+        }
+    
+    }
+    
+    @IBAction func expandButtonTapped(sender: AnyObject) {
+        
+        if self.isExpanded == false {
+            self.expand()
+        } else {
+            self.shrink()
         }
     }
-
+    
 }
 
